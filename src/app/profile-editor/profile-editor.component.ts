0a707876b583ada6408ed84a08850b5a76fd2f92
xAbdx <67425264+xAbdx@users.noreply.github.com>
2021-02-02 14:07:08 +0200

    s

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 
 @Component({
@@ -9,14 +9,20 @@ import { Validators } from '@angular/forms';
 })
 export class ProfileEditorComponent implements OnInit {
   profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    // firstName: new FormControl(''),
+    // lastName: new FormControl(''),
     name: new FormControl(''),
     email: new FormControl(''),
     gender: new FormControl(''),
     avatar: new FormControl(''),
     mobileNumber: new FormControl('')
   });
+
+  event: any;
+  URL: string | ArrayBuffer | null | undefined;
+  _changeDetection: any;
+
+  constructor(private fb: FormBuilder) { }
 
   // profileForm = ({
   //   firstName: ['', Validators.required],
@@ -27,15 +33,37 @@ export class ProfileEditorComponent implements OnInit {
   //   avatar: ['', Validators.required],
   //   mobileNumber: ['', Validators.required],
   // });
-  
+
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
+    if (!this.profileForm.value) {
+      alert("you have to fill all the feilds")
+    }
   }
-
-  constructor() { }
 
   ngOnInit(): void {
+    this.profileForm = new FormGroup({
+      name: new FormControl(this.profileForm.name, [
+        Validators.required,
+        Validators.minLength(3),
+
+      ])
+    })
+
   }
 
+  
+
+  useImage(event: { target: { files: Blob[]; }; }) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // Read file as data url
+      reader.onloadend = (e) => { // function call once readAsDataUrl is completed
+        this.URL = e.target['result']; // Set image in element
+        this._changeDetection.markForCheck(); // Is called because ChangeDetection is set to onPush
+      };
+    }
+  }
 }
